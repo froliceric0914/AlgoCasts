2a693e6460a176bb6287573612baa21574c8d01b
froliceric0914 <wzhao.eric@gmail.com>
2020-09-20 14:09:25 -0400

    use reduce for reverseStr and ttry debugger

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//reduce Array helper
+// .reduce((args1,args2)=>{},"")
+// the logic is the same as the reverse2() function: iterate through the array
+// and move the former element to the back
 
-module.exports = reverse;
+function reverse3(str) {
+    debugger;
+    // run node inspect index.jx
+    // type in repl, and it enables us to audit the function line by line
+    return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+reverse3('abcd');
+
+module.exports = reverse3;
+
+// reverse() is the an Array method
+// thus we need to turn the string into an array, and then join() it back as a string
+// eventually we return to the result
+// function reverse(str) {
+//     return arr.split('').reverse().join('');
+// }
+
+// this is a stack-kind thought
+// loop through the string, and add the element to the back
+// function reverse2(str) {
+//     let reverse = '';
+//     for (let character of str) {
+//         reverse = character + reverse;
+//     }
+//     return reverse;
+// }
