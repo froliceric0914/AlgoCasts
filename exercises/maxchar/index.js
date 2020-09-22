1d8c27cd38dfc3fd0ea97532c68b1e4ded69a8a1
froliceric0914 <wzhao.eric@gmail.com>
2020-09-21 22:52:04 -0400

    solve maxChar; refresh the Object methods

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+//build an object with the key for the letter and value for the frequency
+
+// for...of is for string/array
+// for...in is for object(key/value pairs)
+function maxChar(str) {
+    let charLib = {};
+    for (let e of str) {
+        // if (!charLib[e]) {
+        //     charLib[e] = 1;
+        // } else {
+        //     charLib[e]++;
+        // }
+
+        //elimiate the usage of the if statement
+        charLib[e] = charLib[e] + 1 || 1;
+    }
+    // Object.values return to the array of values of the object
+    // Array.reduce() is very useful to get the the only desired value
+    const maxNum = Object.values(charLib).reduce((a, b) => Math.max(a, b));
+
+    // Object.keys return to the array of keys of the object
+    return Object.keys(charLib).find((key) => charLib[key] === maxNum);
+}
 
 module.exports = maxChar;
